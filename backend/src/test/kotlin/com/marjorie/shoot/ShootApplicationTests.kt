6b4461fa7dc60bd253dc7d67355eae49c8d30f05
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2022-10-15 18:09:17 +0300

    Init

---
+++ backend/src/test/kotlin/com/marjorie/shoot/ShootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marjorie.shoot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
