966d82c656823954e3013d447bb0f273416d50eb
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2023-01-14 17:49:34 +0200

    Add backend's Dockerfile

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 //======================= Project Info =============================================
 group = "com.marjorie"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
